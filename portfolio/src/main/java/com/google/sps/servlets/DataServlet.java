222e4cfa4a6163847457285cfac93e9c8fe4c383
Lilian Hunt <lilian.hunt803@gmail.com>
2020-12-15 01:45:57 +0000

    Fix spacing DataServlet.java

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -76,9 +76,7 @@ public class DataServlet extends HttpServlet {
     if (feedback == "") {
       LOGGER.warning("No input");
       return;
-    }
-
-    else {
+    } else {
       Entity commentEntity = new Entity("Comment");
 
       commentEntity.setProperty("comment", feedback);
