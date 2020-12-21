b50619e3d2710bb09b9e2a337a8455b30a4e1f3d
Lilian Hunt <lilian.hunt803@gmail.com>
2020-12-21 04:39:03 +0000

    Find time intervals when users are busy and merge these intervals

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,39 @@
 
 package com.google.sps;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
+import java.util.Timer;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    Collection<String> attendees = request.getAttendees();
+    long duration = request.getDuration();
+    List<TimeRange> busyTimes = new ArrayList<>();
+    for (Event event : events) {
+      busyTimes.add(event.getWhen());
+    }
+    busyTimes.sort(TimeRange.ORDER_BY_START);
+    List<TimeRange> mergedBusyTimes = new ArrayList<>();
+    for (TimeRange timeRange : busyTimes) {
+      // If the merged list is empty or the new interval does not overlap, append it.
+      // Otherwise, there must be overlap so we create a new TimeRange for the merged intervals.
+      if (mergedBusyTimes.size() == 0
+          || !(mergedBusyTimes.get(mergedBusyTimes.size() - 1)).overlaps(timeRange)) {
+        mergedBusyTimes.add(timeRange);
+      } else {
+        TimeRange lastFromMergedTimes = mergedBusyTimes.remove(mergedBusyTimes.size() - 1);
+        TimeRange mergedRange = TimeRange.fromStartEnd(lastFromMergedTimes.start(),
+            Math.max(lastFromMergedTimes.end(), timeRange.end()), false);
+        mergedBusyTimes.add(mergedRange);
+      }
+    }
+    // Find a gap equal to the duration.
+    List<TimeRange> availableTimes = new ArrayList<>();
+    for (TimeRange timeRange : mergedBusyTimes) {
+      System.out.println(timeRange.toString());
+    }
+    // throw new UnsupportedOperationException("TODO: Implement this method.");
   }
 }
