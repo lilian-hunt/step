f8e9a182d46501e4a628bb0bcb66d870c434fc29
Lilian Hunt <lilian.hunt803@gmail.com>
2020-12-23 02:03:40 +0000

    Modify data servlet to store image url with comment

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -14,6 +14,11 @@
 
 package com.google.sps.servlets;
 
+import com.google.appengine.api.blobstore.BlobInfo;
+import com.google.appengine.api.blobstore.BlobInfoFactory;
+import com.google.appengine.api.blobstore.BlobKey;
+import com.google.appengine.api.blobstore.BlobstoreService;
+import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
@@ -21,10 +26,15 @@ import com.google.appengine.api.datastore.KeyFactory;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.appengine.api.images.ImagesService;
+import com.google.appengine.api.images.ImagesServiceFactory;
+import com.google.appengine.api.images.ServingUrlOptions;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 import com.google.gson.Gson;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -51,10 +61,11 @@ public class DataServlet extends HttpServlet {
     Map<String, Map<String, String>> comments = new HashMap<>();
     for (Entity entity : results.asIterable()) {
       String id = KeyFactory.keyToString(entity.getKey());
-      Map<String, String> commentEmail = new HashMap<>();
-      commentEmail.put("comment", (String) entity.getProperty("comment"));
-      commentEmail.put("userEmail", (String) entity.getProperty("userEmail"));
-      comments.put(id, commentEmail);
+      Map<String, String> commentAttributes = new HashMap<>();
+      commentAttributes.put("comment", (String) entity.getProperty("comment"));
+      commentAttributes.put("userEmail", (String) entity.getProperty("userEmail"));
+      commentAttributes.put("imageUrl", (String) entity.getProperty("imageUrl"));
+      comments.put(id, commentAttributes);
     }
 
     response.setContentType("application/json");
@@ -77,6 +88,8 @@ public class DataServlet extends HttpServlet {
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     // Get the input from the form.
     String feedback = request.getParameter("text-input");
+    // Get the URL of the image that the user uploaded to Blobstore.
+    String imageUrl = getUploadedFileUrl(request, "image");
     long timestamp = System.currentTimeMillis();
     String userEmail = userService.getCurrentUser().getEmail();
 
@@ -85,16 +98,55 @@ public class DataServlet extends HttpServlet {
       LOGGER.warning("No input");
       return;
     } else {
+    if (feedback != "" || imageUrl != null) {
       Entity commentEntity = new Entity("Comment");
 
       commentEntity.setProperty("comment", feedback);
       commentEntity.setProperty("timestamp", timestamp);
       commentEntity.setProperty("userEmail", userEmail);
 
+      if (imageUrl != null) {
+        commentEntity.setProperty("imageUrl", imageUrl);
+      } else {
+        commentEntity.setProperty("imageUrl", "null");
+      }
       datastore.put(commentEntity);
     }
 
     // Redirect back to the HTML page.
     response.sendRedirect("/index.html");
+  }
+  /** Returns a URL that points to the uploaded file, or null if the user didn't upload a file. */
+  private String getUploadedFileUrl(HttpServletRequest request, String formInputElementName) {
+    BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
+    Map<String, List<BlobKey>> blobs = blobstoreService.getUploads(request);
+    List<BlobKey> blobKeys = blobs.get("image");
+
+    // User submitted form without selecting a file, so we can't get a URL. (dev server)
+    if (blobKeys == null || blobKeys.isEmpty()) {
+      return null;
+    }
+
+    // Our form only contains a single file input, so get the first index.
+    BlobKey blobKey = blobKeys.get(0);
+
+    // User submitted form without selecting a file, so we can't get a URL. (live server)
+    BlobInfo blobInfo = new BlobInfoFactory().loadBlobInfo(blobKey);
+    if (blobInfo.getSize() == 0) {
+      blobstoreService.delete(blobKey);
+      return null;
+    }
+
+    // Use ImagesService to get a URL that points to the uploaded file.
+    ImagesService imagesService = ImagesServiceFactory.getImagesService();
+    ServingUrlOptions options = ServingUrlOptions.Builder.withBlobKey(blobKey);
+
+    // Use the relative path to the image.
+    try {
+      URL url = new URL(imagesService.getServingUrl(options));
+      return url.getPath();
+    } catch (MalformedURLException e) {
+      return imagesService.getServingUrl(options);
+    }
   }
 }
