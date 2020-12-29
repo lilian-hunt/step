0bc2859898081aa0bf8e4a4937bac5edb7304799
Lilian Hunt <lilian.hunt803@gmail.com>
2020-12-29 02:12:12 +0000

    Modify dataservlet to store photo if uploaded

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -22,7 +22,7 @@ function revealFact() {
       'I went viral on TikTok for ski dance videos.', 'I love baking.',
       'My favourite chocolate is Caramilk.', 'I play water polo.',
       'I went to Stanford University on exchange.',
-      'I have worked at Deloitte.', 'I have worked at Macqaurie bank.',
+      'I have worked at Deloitte.', 'I have worked at Macqaurie bank.'
     ];
 
     // Pick a random one.
@@ -47,11 +47,11 @@ function revealFact() {
 function getFeedback() {
   fetch('/data').then(response => response.json()).then((comments) => {
     // comments is an object, so reference its fields to create HTML content
-    const commentsEntries = comments.comments;
+    var comments = comments.comments;
 
     const commentListElement = document.getElementById('comment-container');
     commentListElement.innerHTML = '';
-    for (const [key, value] of Object.entries(commentsEntries)) {
+    for (const [key, value] of Object.entries(comments)) {
       commentListElement.appendChild(createListElement(key, value));
     }
   });
@@ -61,34 +61,39 @@ function getFeedback() {
 function createListElement(key, text) {
   const liElement = document.createElement('li');
   liElement.id = key;
-  liElement.innerText = text.comment + ',' + text.userEmail + '\t';
+  liElement.innerText = text.commentText + ',' + text.userEmail + '\t';
+
+  if (text.imageUrl != 'null') {
+    const img = document.createElement('img');
+    img.src = text.imageUrl;
+    liElement.appendChild(img);
+  }
 
   // Create an input element to delete item.
-  const form = document.createElement('form');
+  var form = document.createElement('form');
   form.setAttribute('method', 'post');
   form.setAttribute('action', '/delete-data');
 
-  // Create hidden element to store comment id (unique key used to 
-  // identify the commment in the database) and link it to the button.
-  const comment_id = document.createElement('input');
-  comment_id.setAttribute('type', 'hidden');
-  comment_id.setAttribute('name', 'comment_id');
-  comment_id.setAttribute('value', key);
+  // Create hidden element to store id of button.
+  var id = document.createElement('input');
+  id.setAttribute('type', 'hidden');
+  id.setAttribute('name', 'id');
+  id.setAttribute('value', key);
 
   // Create hidden element to store user email.
-  const userEmail = document.createElement('input');
+  var userEmail = document.createElement('input');
   userEmail.setAttribute('type', 'hidden');
   userEmail.setAttribute('name', 'userEmail');
   userEmail.setAttribute('value', text.userEmail);
 
-  const button = document.createElement('input');
+  var button = document.createElement('input');
   button.setAttribute('class', 'btn btn-outline-secondary');
   button.setAttribute('type', 'submit');
   button.setAttribute('name', key);
   button.setAttribute('value', 'X');
 
   // Create the list element
-  form.appendChild(comment_id);
+  form.appendChild(id);
   form.appendChild(userEmail);
   form.appendChild(button);
   liElement.appendChild(form);
@@ -98,43 +103,52 @@ function createListElement(key, text) {
 
 // Check if the user is logged in or not, only display the comment function
 // if they are logged in.
-const xhttp = new XMLHttpRequest();
+var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
-    const commentTitle = document.getElementById('comment-header');
-    const loginButton = document.getElementById("login-button");
+    commentTitle = document.getElementById('comment-header');
     if (xhttp.responseText.includes('<p>You are logged in!</p>')) {
-      const form = document.createElement('form');
+      var form = document.createElement('form');
       form.setAttribute('method', 'POST');
-      form.setAttribute('action', '/data');
+      form.setAttribute('id', 'comment-form');
+      form.setAttribute('class', 'hidden');
+      form.setAttribute('enctype', 'multipart/form-data');
 
-      const feedbackPrompt = document.createElement('p');
-      feedbackPrompt.innerText = 'Enter your feedback here:';
+      var text = document.createElement('p');
+      text.innerText = 'Enter your feedback here:';
 
-      const textArea = document.createElement('textarea');
+      var textArea = document.createElement('textarea');
       textArea.name = 'text-input';
-      textArea.required = '';
+      textArea.required = true;
 
-      const breakElement = document.createElement('br');
+      const imgText = document.createElement('p');
+      imgText.innerText = 'Upload an image:';
 
-      const button = document.createElement('input');
+      const imgInput = document.createElement('input');
+      imgInput.type = 'file';
+      imgInput.name = 'image';
+      imgInput.setAttribute('class', 'btn btn-outline-secondary');
+
+      const br = document.createElement('br');
+      const br2 = document.createElement('br');
+
+      var button = document.createElement('input');
       button.setAttribute('class', 'btn btn-outline-secondary');
       button.setAttribute('type', 'submit');
 
-      form.appendChild(feedbackPrompt);
+      form.appendChild(text);
       form.appendChild(textArea);
-      form.appendChild(breakElement);
+      form.appendChild(imgText);
+      form.appendChild(imgInput);
+      form.appendChild(br);
+      form.appendChild(br2);
       form.appendChild(button);
       commentTitle.parentNode.insertBefore(form, commentTitle.nextSibling);
-
-      loginButton.value = "Logout";
     } else {
-      const text = document.createElement('p');
+      var text = document.createElement('p');
       text.innerHTML = 'Please login to comment.';
       commentTitle.append(text);
       commentTitle.parentNode.insertBefore(text, commentTitle.nextSibling);
-
-      loginButton.value = "Login";
     }
   }
 };
@@ -147,11 +161,24 @@ fetch('./config.json')
       return response.json();
     })
     .then(data => {
-      const script = document.createElement('script');
+      var script = document.createElement('script');
       script.src =
           'https://maps.googleapis.com/maps/api/js?key=' + data.api_key;
-      document.head.appendChild(script);
-      const mapScript = document.createElement('script');
-      mapScript.src = 'map.js'
-      document.head.appendChild(mapScript);
+      script.defer = false;
+      document.head.insertBefore(script, document.head.lastChild);
     });
+
+
+/**  Make a GET request to /blobstore-upload-url. */
+function fetchBlobstoreUrlAndShowForm() {
+  console.log('CALLED');
+  fetch('/blobstore-upload-url')
+      .then((response) => {
+        return response.text();
+      })
+      .then((imageUploadUrl) => {
+        const messageForm = document.getElementById('comment-form');
+        messageForm.action = imageUploadUrl;
+        messageForm.classList.remove('hidden');
+      });
+}
